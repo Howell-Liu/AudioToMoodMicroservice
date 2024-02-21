9df26982e35024acd32e2d910854e5579a1d3256
HLiu <chuan.liu23@imperial.ac.uk>
2024-02-21 17:46:09 +0000

    updated unit test data samples and fixed other parts of the code

--- mp3_to_wav.py
+++ mp3_to_wav.py
@@ -8,4 +8,5 @@ output_file = audio_string+".wav"
   
 # convert mp3 file to wav file 
 sound = AudioSegment.from_mp3(input_file) 
+sound = sound.set_channels(1)
 sound.export(output_file, format="wav")
